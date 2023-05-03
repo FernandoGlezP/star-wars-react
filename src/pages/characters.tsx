7994346ff08b1ce73a3440bb3de1939656a21042
Fernando Gonzalez <fernandodesarollador@gmail.com>
2023-05-03 12:43:57 -0600

    proyecto muerto ☠😪

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -1,41 +1,67 @@
-import type Character from "../interfaces/character"
-import CharacterList from "../components/pages/characters/CharacterList"
+import type Character from "../interfaces/character";
+import CharacterList from "../components/pages/characters/CharacterList";
+import { useEffect, useState } from "react";
 
-const characters: Character[] = [
-  {
-      name: "Luke Skywalker"
-  },
-  {
-      name: "Darth Vader"
-  },
-  {
-      name: "Han Solo"
-  },
-  {
-      name: "Leia Organa"
-  },
-  {
-      name: "Chewbacca"
-  },
-  {
-      name: "Yoda"
-  },
-  {
-      name: "Obi-Wan Kenobi"
-  },
-  {
-      name: "R2-D2"
-  },
-  {
-      name: "C-3PO"
-  },
-  {
-      name: "Boba Fett"
+type TURL = string | null;
+
+function Characters() {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  //const fetchUrl = "https://swapi.dev/api/people/";
+  const [isLoading, setIsLoading] = useState(true);
+  const [fetchUrl, setFetchUrl] = useState<TURL>(
+    "https://swapi.dev/api/people?page=7"
+  );
+
+  const [prevUrl, setPrevUrl] = useState<TURL>(null);
+  const [nextUrl, setNextUrl] = useState<TURL>(null);
+
+  const getCharacters = async () => {
+    if (fetchUrl) {
+      setIsLoading(true);
+      const response = await fetch(fetchUrl);
+      const { results, previous, next } = await response.json();
+      setCharacters(results);
+      setPrevUrl(previous);
+      setNextUrl(next);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // if (fetchUrl) {
+    //   setIsLoading(true);
+    //   fetch(fetchUrl)
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       setCharacters(data.results);
+    //       setPrevUrl(data.previous);
+    //       setNextUrl(data.next);
+    //       setIsLoading(false);
+    //     });
+    // }
+    getCharacters();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fetchUrl]);
+
+  const onPrev = () => {
+    setFetchUrl(prevUrl);
+  };
+
+  const onNext = () => {
+    setFetchUrl(nextUrl);
+  };
+
+  if (isLoading) {
+    return <span>LOADING...</span>;
   }
-]
 
-function Characters(){
-  return <CharacterList characters={characters}/>
+  return (
+    <>
+      <CharacterList characters={characters} />
+      {prevUrl ? <button onClick={onPrev}>previous</button> : null}
+      {nextUrl ? <button onClick={onNext}>next</button> : null}
+    </>
+  );
 }
 
-export default Characters
+export default Characters;
